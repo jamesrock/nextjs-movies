b2a65c3a8cf9f958b970ed208f05994afe5cc758
James Rock <hello@jamesrock.me>
2025-09-05 22:45:37 +0100

    updates

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Category from './components/Category';
 export default async function Home() {
   return (
     <main>
+      <input type="search" placeholder="Search film, actor, director" />
       <div className="categories">
         <Category name="Latest releases" />
         <Category id="Comedy" name="Comedy" />
