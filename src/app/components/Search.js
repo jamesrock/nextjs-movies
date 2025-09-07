7f1f578672e949b3cd52a5385b90f67474783585
James Rock <hello@jamesrock.me>
2025-09-07 19:54:51 +0100

    updates

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -3,26 +3,36 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import { fetch_options, tmdb_base, tmdb_media_base, media_type_name, media_type_profile_path, filterSearch } from '@/app/api';
+const cache = {};
 
 export default function Search() {
   const [list, setList] = useState([]);
-  const runSearch = (query) => {
-    if(query.length === 0) {
-      setList([]);
+  const [query, setQuery] = useState('');
+  const runSearch = (q) => {
+    setQuery(q);
+    if(q.length === 0) {
+      clearSearch();
       return;
+    };
+    if(cache[q]) {
+      return setList(cache[q]);
     };
     fetch(tmdb_base + `/search/multi?query=${query}&include_adult=false&language=en-US&page=1`, fetch_options)
       .then(response => response.json())
-      .then(data => setList(filterSearch(data.results)))
+      .then(data => {cache[q] = filterSearch(data.results); setList(cache[q]);})
       .catch(error => console.log('Error:', error));
+  };
+  const clearSearch = () => {
+    setQuery('');
+    setList([]);
   };
   return (
     <div className="search">
       <div className="search-body">
-        <input type="search" placeholder="Search film, actor, director" onChange={(e) => runSearch(e.target.value)} />
+        <input type="search" placeholder="Search film, actor, director" value={query} onChange={(e) => runSearch(e.target.value)} />
         <div className="search-results">
           {list.map((item) => (
-            <Link href={`/${item.media_type}/${item.id}`} className="search-results-item" key={item.id}>
+            <Link href={`/${item.media_type}/${item.id}`} className="search-results-item" key={item.id} onClick={clearSearch}>
               <div className="poster-wrap">
                 <div className="poster-crop">
                   <img className="poster" src={item[media_type_profile_path[item.media_type]] ? (tmdb_media_base + item[media_type_profile_path[item.media_type]]) : null} />
