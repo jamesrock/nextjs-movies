d9763bbfd5fb8eca10f937ce68f5505ad1630b1c
James Rock <hello@jamesrock.me>
2025-09-10 10:31:11 +0100

    updates

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import { fetch_options, tmdb_base, tmdb_media_base, media_type_name, media_type_profile_path, addProp, sortByProp, filterByMatch } from '@/app/api';
+import Poster from './Poster';
 const cache = {};
 
 export default function Search() {
@@ -40,9 +41,7 @@ export default function Search() {
           {list.map((item) => (
             <Link href={`/${item.media_type}/${item.id}`} className="search-results-item" key={item.id} onClick={clearSearch}>
               <div className="poster-wrap">
-                <div className="poster-crop">
-                  <img className="poster" src={item[media_type_profile_path[item.media_type]] ? (tmdb_media_base + item[media_type_profile_path[item.media_type]]) : null} />
-                </div>
+                <Poster src={item[media_type_profile_path[item.media_type]]} />
               </div>
               <div>{item[media_type_name[item.media_type]]}</div>
             </Link>
