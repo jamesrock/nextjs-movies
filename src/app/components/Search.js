0e6b34694ae9988564bc930f8a6a097676c8d88f
James Rock <hello@jamesrock.me>
2025-09-17 17:57:52 +0100

    updates

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -7,7 +7,7 @@ import Poster from './Poster';
 const cache = {};
 
 export default function Search() {
-  const [list, setList] = useState([]);
+  const [items, setItems] = useState([]);
   const [query, setQuery] = useState('');
   const runSearch = (q) => {
     setQuery(q);
@@ -16,25 +16,24 @@ export default function Search() {
       return;
     };
     if(cache[q]) {
-      setList(cache[q]);
+      setItems(cache[q]);
       return;
     };
     api.search(q).then(data => {
       cache[q] = sortByProp(filterByMatch([...addProp(data[0].results, 'media_type', 'movie'), ...addProp(data[1].results, 'media_type', 'person')], q), 'popularity');
-      setList(cache[q]);
-      // console.log('combined', cache[q]);
+      setItems(cache[q]);
     }).catch(error => console.log('Error:', error));
   };
   const clearSearch = () => {
     setQuery('');
-    setList([]);
+    setItems([]);
   };
   return (
     <div className="search">
       <div className="search-body">
         <input type="search" placeholder="Search film, actor, director" value={query} onChange={(e) => runSearch(e.target.value)} />
         <div className="search-results">
-          {list.map((item) => (
+          {items.map((item) => (
             <Link href={`/${item.media_type}/${item.id}`} className="search-results-item" key={item.id} onClick={clearSearch}>
               <div className="poster-wrap">
                 <Poster path={item[media_type_profile_path[item.media_type]]} size={largest_size_map[item.media_type]} />
