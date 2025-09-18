2240dd50f81b0fc8c140ed136d7e13ecb8e555cd
James Rock <hello@jamesrock.me>
2025-09-18 08:57:25 +0100

    updates

--- src/app/components/Navigation.js
+++ src/app/components/Navigation.js
@@ -1,12 +1,11 @@
 'use client'
 
-import { ImHome3 } from 'react-icons/im';
 import Link from 'next/link';
 const items = [
   {
     'name': 'Home',
     'href': '/',
-    'icon': <ImHome3 />
+    'icon': <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 16 16"><path fill="currentColor" d="m16 9.226l-8-6.21l-8 6.21V6.694l8-6.21l8 6.21zM14 9v6h-4v-4H6v4H2V9l6-4.5z"/></svg>
   }
 ];
 
@@ -15,7 +14,7 @@ export default function Navigation() {
     <div className="navigation">
       <div className="navigation-body">
         {items.map((item, i) => (
-          <Link href={item.href} className="navigation-item" key={`navogation-${i}`}>
+          <Link href={item.href} aria-label={item.name} className="navigation-item" key={`navogation-${i}`}>
             {item.icon}
           </Link>
         ))}
